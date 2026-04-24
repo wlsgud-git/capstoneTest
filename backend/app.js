eaf2907a554d3c40d0b2d0ded6fba9348517bfee
jins <fcipras8694@naver.comm>
2026-04-24 14:04:13 +0900

    ASdasdasd

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
